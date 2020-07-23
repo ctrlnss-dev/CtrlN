5b66f3f6f491d95a6aaa052589043fe176109ba2
xeen0 <sahitsrivyshnav1999@gmail.com>
2020-07-23 06:48:36 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./Components/Home/Home";
-import NavbarC from "./Components/Navbar/NavbarC";
-import OurWork from "./Components/Our Work/ourWork";
-import ContactUs from "./Components/ContactUs/ContactUs";
-import Cardc from "./Components/cards/cardc";
+
 import LayoutComponent from "./Components/Layout/Layout";
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Cardc/> */}
-      {/* <NavbarC/> */}
+      
       <LayoutComponent/>
     </div>
   );
