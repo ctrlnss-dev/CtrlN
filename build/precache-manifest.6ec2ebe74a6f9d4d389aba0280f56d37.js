0a489e28b3466ff19973f4fff8bf45bb975900de
xeen0 <sahitsrivyshnav1999@gmail.com>
2020-07-29 23:08:50 +0530

    Production Release V1.0

---
+++ build/precache-manifest.6ec2ebe74a6f9d4d389aba0280f56d37.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00449824664d29e40724545dd0f51ff",
+    "url": "/CtrlN/index.html"
+  },
+  {
+    "revision": "3d14738336e611f34249",
+    "url": "/CtrlN/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cbc7f28bb2d32d12e73f",
+    "url": "/CtrlN/static/css/main.267ec30e.chunk.css"
+  },
+  {
+    "revision": "3d14738336e611f34249",
+    "url": "/CtrlN/static/js/2.92a44d2a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/CtrlN/static/js/2.92a44d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc7f28bb2d32d12e73f",
+    "url": "/CtrlN/static/js/main.ed9ac6b7.chunk.js"
+  },
+  {
+    "revision": "541c84b57034dff9ff0b",
+    "url": "/CtrlN/static/js/runtime-main.5bf782c3.js"
+  },
+  {
+    "revision": "635fc6e22844cbbbd141f2fa1c6cb9be",
+    "url": "/CtrlN/static/media/CtrlN.635fc6e2.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CtrlN/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f645f957bc22eb91e01585d7f53021ed",
+    "url": "/CtrlN/static/media/u5a_logo.f645f957.svg"
+  }
+]);
